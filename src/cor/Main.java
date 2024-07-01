16ce87b901545372ad2057f0de492b30999ffb7b
DaniAmmi <danieleammirevole@gmail.com>
2024-07-01 18:51:24 +0200

    First Release

---
+++ src/cor/Main.java
@@ -0,0 +1,8 @@
+package cor;
+import core.fire.Charizard;
+
+public class Main {
+	private static void main(String[] args) {
+		Charizard charizard = new Charizard(theName: "cucciolo", theLevel: 77, theWeakness: "acqua");
+	}
+}
---
+++ src/core/fire/Charizard.java
@@ -0,0 +1,16 @@
+package core.fire;
+
+public class Charizard {
+	private String name;
+	private Integer level;
+	private String weakness;
+	
+	public Charizard(String theName, Integer theLevel, String theWeakness) {
+		System.out.println("AHHHHHHHHHH");
+		name = theName;
+		level = theLevel;
+		weakness = theWeakness;
+		System.out.println("che nome terribile mi hai dato? mi chiamo" + theName);
+
+	}
+}
